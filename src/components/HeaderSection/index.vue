41356e8268b53948a2d938e0b2e067ba0c7a3f46
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-09-18 11:13:06 +0300

    enable CSS Modules with SCSS syntax for HeaderSection

--- src/components/HeaderSection/index.vue
+++ src/components/HeaderSection/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1>Header</h1>
+    <h1 :class="$style.hero">Header</h1>
   </div>
 </template>
 
 <script>
-import styles from './HeaderSection.module.scss';
-
 export default {
   name: 'header-section',
 };
 </script>
+
+<style src="./HeaderSection.module.scss" lang="scss" module />
