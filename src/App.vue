a28dc15e8af7d3a59af41d980741aa3cd351736f
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-09-19 17:14:53 +0300

    add footer, sprite and convert units to rem

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <header-section />
     <router-view />
+    <footer-section />
   </div>
 </template>
 
 <script>
 import HeaderSection from '@/components/HeaderSection';
+import FooterSection from '@/components/FooterSection';
 
 export default {
   components: {
     HeaderSection,
+    FooterSection,
   },
 };
 </script>
---
+++ src/components/FooterSection/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer :class="$style.footer">
+    <div class="container">
+      <div :class="$style.wrapper">
+        <span>&lt;/&gt; with</span>
+        <span class="sprite sprite-vue" />
+        <span>and</span>
+        <span class="sprite sprite-heart" />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import content from '@/content.json';
+
+export default {
+  name: 'footer-section',
+  data: () => ({
+    isOpen: false,
+    content,
+  }),
+};
+</script>
+
+<style src="./FooterSection.module.scss" lang="scss" module />
