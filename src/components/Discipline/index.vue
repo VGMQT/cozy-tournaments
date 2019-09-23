0216f0e2291a594d8c43bffe62b573604f13dcb2
a.skorohod <a.skorohod@ydt-global.com>
2019-09-23 18:27:15 +0300

    Created discipline component

---
+++ src/components/Discipline/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.discipline">
+    <img src="@/assets/table_tennis.png" alt="" />
+    <a :class="description" :href="link"
+      ><h2>{{ title }}</h2></a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'discipline',
+  props: {
+    link: String,
+    title: String,
+    //imageSrc: String,
+  },
+};
+</script>
+
+<style src="./Discipline.module.scss" lang="scss" module />
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,12 +2,14 @@
   <div class="container">
     <page-title :title="content.title" />
     <page-desc :desc="content.desc" />
+    <discipline :imageSrc="'./images/tt.png'" :link="'/'" :title="'Tennis'" />
   </div>
 </template>
 
 <script>
 import PageTitle from '@/elements/PageTitle';
 import PageDesc from '@/elements/PageDesc';
+import Discipline from '@/components/Discipline';
 import content from '@/content.json';
 
 export default {
@@ -15,6 +17,7 @@ export default {
   components: {
     PageTitle,
     PageDesc,
+    Discipline,
   },
   data: () => ({
     content: content.homepage,
