b5f7062b90ec9c2ea91367a758a6f4cab2858950
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-09-24 16:57:59 +0300

    merge users and auth in one

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,30 +1,40 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User', new mongoose.Schema({
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
     email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1024
-    }
-}));
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
+  })
+);
 
-function validateUser(user) {
-    const schema = {
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    };
+const validateUser = user => {
+  const schema = {
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+  };
 
-    return Joi.validate(user, schema);
-}
+  return Joi.validate(user, schema);
+};
 
 exports.User = User;
 exports.validate = validateUser;
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,52 +1,42 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const { User } = require('../models/user');
+const { User, validate } = require('../models/user');
 const express = require('express');
+
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  // First Validate The HTTP Request
+  // Validate The Request
   const { error } = validate(req.body);
+
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  // Now find the user by their email address
+  // Check if this user already exists
   let user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    return res.status(400).send('Incorrect email or password.');
+
+  if (user) {
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).send('Incorrect email or password.');
+    }
+
+    const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
+
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'email']));
+  } else {
+    // Insert the new user if they do not exist yet
+    user = new User(_.pick(req.body, ['email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'email']));
   }
-
-  // Then validate the Credentials in MongoDB match
-  // those provided in the request
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-  if (!validPassword) {
-    return res.status(400).send('Incorrect email or password.');
-  }
-
-  const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
-
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'email']));
 });
-
-function validate(req) {
-  const schema = {
-    email: Joi.string()
-      .min(5)
-      .max(255)
-      .required()
-      .email(),
-    password: Joi.string()
-      .min(5)
-      .max(255)
-      .required(),
-  };
-
-  return Joi.validate(req, schema);
-}
 
 module.exports = router;
