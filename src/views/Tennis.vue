a2257de319b4437079916f5809b4f232b819e9c8
a.skorohod <a.skorohod@ydt-global.com>
2019-09-25 13:55:25 +0300

    Added leaders table with sorting

--- src/views/Tennis.vue
+++ src/views/Tennis.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
     <h1>Table Tennis</h1>
+    <leaders-table :leaders="leaders" />
   </div>
 </template>
 
 <script>
+import content from '@/content.json';
+import LeadersTable from '../components/LeaderTable/index';
+
 export default {
   name: 'tennis',
+  components: { LeadersTable },
+  data: () => ({
+    leaders: content.disciplines[0].leaders,
+  }),
 };
 </script>
